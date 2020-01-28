622bcf67f3e6022c9163713461613ccf3351bf8f
MRI <matija.risek@kogitas.com>
2020-01-28 09:16:48 +0100

    first commit

---
+++ AutomatedTesting/AutomatedTesting/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AutomatedTesting.Models;
+
+namespace AutomatedTesting.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
